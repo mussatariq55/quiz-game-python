07183fd65188e3e50de6b1dcb7787d6a7a11d318
Muhammad Mussa Tariq <166991480+mussatariq55@users.noreply.github.com>
2025-07-29 16:20:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
-from art import logo
+from ui import QuizInterface
 
 question_bank = []
-for qna in question_data:
-    question_bank.append(Question(qna["text"], qna["answer"]))
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
 
 quiz = QuizBrain(question_bank)
-print(logo)
-while quiz.still_has_question():
-    quiz.next_question()
-print("Congrats! You have completed the quiz")
-print(f"Your final score was: {quiz.score}/{len(question_bank)}")
+ui = QuizInterface(quiz)
+
+# while quiz.still_has_questions():
+#     quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
